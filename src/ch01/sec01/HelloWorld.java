b001d42e5f1e212f65d01af482a8d2133a4bc844
이성희 <sh01094701600@etomato.com>
2017-02-15 11:11:45 +0900

    자바 테스트

---
+++ src/ch01/sec01/HelloWorld.java
@@ -0,0 +1,10 @@
+package ch01.sec01;
+
+/**
+ * Created by 이성희 on 2017-02-15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
---
+++ src/ch01/sec01/MethodDemo.java
@@ -0,0 +1,13 @@
+package ch01.sec01;
+
+/**
+ * Created by 이성희 on 2017-02-15.
+ */
+public class MethodDemo {
+    public static void main(String[] args) {
+        System.out.println("hello".length());
+
+
+
+    }
+}
